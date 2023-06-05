c2b15c1121752b2f073b2bd6f1ecb0f0232fc47b
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-05 12:50:02 +0200

    refactor: Rename file

---
+++ packages/ticket_sidebar/src/ticket_autocomplete.ts
@@ -0,0 +1,43 @@
+import zafClient from "@app/zendesk/sdk"
+
+const MINIMUM_ORDER_ID_LENGTH = 6
+
+type Ticket = {
+    id: number,
+}
+
+type hasGreaterFn = (len: number) => (str: string) => boolean
+const hasLengthGreaterThan: hasGreaterFn = (len: number) => (str: string) => str.length >= len
+
+const hasLengthGreaterThanN = hasLengthGreaterThan(MINIMUM_ORDER_ID_LENGTH)
+
+export async function init() {
+  function showError(error: Error) {
+      console.log("Errores Request", error)
+  }
+
+  zafClient.on('ticket.custom_field_360017010219.changed', async function(order_id) {
+      console.log("Se ha modificado el pedido", order_id);
+        const ticket: Ticket = await zafClient.get("ticket")
+        const ticket_id = ticket.id
+
+      if (order_id && hasLengthGreaterThanN(order_id)) {
+          console.log("Order id identificado: ", order_id)
+          const settings = {
+              url: 'http://34.110.237.158/zendesk-hook',
+              type:'POST',
+              contentType: 'application/json',
+              data: JSON.stringify( {order_id, ticket_id, acmofy: true} )
+          };
+
+          zafClient.request(settings).then(
+              function(data) {
+                  console.log("Request Executed", data)
+              },
+              function(response) {
+                  showError(response);
+              }
+          );
+      }
+  });
+}

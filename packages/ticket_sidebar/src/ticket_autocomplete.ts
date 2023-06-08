f7e449d1f0f2de2815a35b2ef8a9265a64f79a60
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-08 12:23:24 +0200

    chore: Prepare app for prod environment

--- packages/ticket_sidebar/src/ticket_autocomplete.ts
+++ packages/ticket_sidebar/src/ticket_autocomplete.ts
@@ -1,9 +1,9 @@
 import zafClient from '@app/zendesk/sdk'
 import { hasLengthGreaterOrEqualThan } from './utils'
 
-const MINIMUM_ORDER_ID_LENGTH = 5
-const ORDER_ID_CUSTOM_FIELD_ID = 360017010219
-const URL_ZENDESK_HOOK = 'http://34.110.237.158/zendesk-hook'
+const MINIMUM_ORDER_ID_LENGTH = 8
+const ORDER_ID_CUSTOM_FIELD_ID = 360012062392
+const URL_ZENDESK_HOOK = 'https://zendeskhook.mercadona.es/zendesk-hook'
 
 type Ticket = {
   id: number
@@ -18,34 +18,36 @@ const hasLengthGreaterThanN = hasLengthGreaterOrEqualThan(
   MINIMUM_ORDER_ID_LENGTH
 )
 
-export async function init() {
-  zafClient.on(
-    'ticket.custom_field_' + ORDER_ID_CUSTOM_FIELD_ID + '.changed',
-    async function (order_id: any) {
-      const { ticket }: TicketResponse = await zafClient.get('ticket')
-      const ticket_id = ticket.id
+const processOrderId = async function (order_id: string) {
+  const { ticket }: TicketResponse = await zafClient.get('ticket')
+  const ticket_id = ticket.id
 
-      console.log('Tengo order id', order_id)
-      if (order_id && hasLengthGreaterThanN(order_id)) {
-        const settings = {
-          url: URL_ZENDESK_HOOK,
-          type: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify({ order_id, ticket_id, acmofy: true }),
-        }
+  console.log('Tengo order id', order_id)
+  if (order_id && hasLengthGreaterThanN(order_id)) {
+    const settings = {
+      url: URL_ZENDESK_HOOK,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ order_id, ticket_id, acmofy: true }),
+    }
 
-        console.log('Ejecutando request')
+    console.log('Ejecutando request')
 
-        try {
-          await zafClient.request(settings)
-        } catch (error: unknown) {
-          if (error instanceof Error) {
-            console.log(error.message)
-          } else {
-            console.log(error)
-          }
-        }
+    try {
+      await zafClient.request(settings)
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error.message)
+      } else {
+        console.log(error)
       }
     }
+  }
+}
+
+export async function init() {
+  zafClient.on(
+    `ticket.custom_field_${ORDER_ID_CUSTOM_FIELD_ID}.changed`,
+    processOrderId
   )
 }

88ccd84896f283441574f813ac583b599d8ee915
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-12 14:04:52 +0200

    feat: Basic HttpClient

---
+++ packages/ticket_sidebar/src/HttpClient.ts
@@ -0,0 +1,26 @@
+import zafClient from '@app/zendesk/sdk'
+
+type ZendeskClient = typeof zafClient
+
+class HttpClient {
+  private client: ZendeskClient
+  private zendeskURL: string
+
+  public constructor(zafClient: ZendeskClient, zendeskURL: string) {
+    this.client = zafClient
+    this.zendeskURL = zendeskURL
+  }
+
+  request(data: object): Promise<unknown> {
+    const settings = {
+      url: this.zendeskURL,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+    }
+
+    return this.client.request(settings)
+  }
+}
+
+export default HttpClient
---
+++ packages/ticket_sidebar/src/tests/HttpClient.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it, vi, afterEach } from 'vitest'
+import HttpClient from '../HttpClient'
+import zafClient from '@app/zendesk/sdk'
+
+vi.mock('@app/zendesk/sdk', async () => {
+  vi.stubGlobal('window', {
+    ZAFClient: {
+      init: vi.fn(),
+    },
+  })
+  return {
+    default: {
+      request: vi.fn(),
+    },
+  }
+})
+
+describe('HttpClient tests', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should construct an instance of HttpClient', () => {
+    const zendeskURL = 'https://example.com'
+    const httpClient = new HttpClient(zafClient, zendeskURL)
+
+    expect(httpClient).toBeInstanceOf(HttpClient)
+  })
+
+  it('should call client with correct parameters', async () => {
+    const zendeskURL = 'https://example.com'
+    const httpClient = new HttpClient(zafClient, zendeskURL)
+
+    const data = { order_id: 1, ticket_id: 2112, acmofy: true }
+
+    await httpClient.request(data)
+
+    expect(zafClient.request).toHaveBeenCalledOnce()
+    expect(zafClient.request).toHaveBeenCalledWith({
+      url: 'https://example.com',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+    })
+  })
+})

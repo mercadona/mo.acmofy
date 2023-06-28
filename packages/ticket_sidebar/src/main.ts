9622599c25b31094ce015f70c17a6859e26d0261
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-28 16:16:22 +0200

    refactor!: Migrate app to React

--- packages/ticket_sidebar/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { init } from './ticket_autocomplete'
-
-document.querySelector('#app')!.innerHTML = ``
-
-init()
--- packages/ticket_sidebar/vite.config.ts
+++ packages/ticket_sidebar/vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vitest/config'
 import * as pluginPkg from '@app/zendesk/vite-plugin-inject-zaf-html/index.js'
 const { injectZafHtmlPlugin } = pluginPkg.default
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   test: {},
-  plugins: [injectZafHtmlPlugin()],
+  plugins: [react(), injectZafHtmlPlugin()],
   base: './',
   build: {
     outDir: `${process.env.INIT_CWD}/dist/assets/${process.env.ADDON_TYPE}`,

82c37a81f1874a9019c91af4b1c4381ca23d4449
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-28 16:15:14 +0200

    feat: Add get method for HttpClient

--- packages/ticket_sidebar/src/HttpClient.ts
+++ packages/ticket_sidebar/src/HttpClient.ts
@@ -21,6 +21,15 @@ class HttpClient {
 
     return this.client.request(settings)
   }
+
+  get(): Promise<unknown> {
+    const settings = {
+      url: this.zendeskURL,
+      type: 'GET',
+    }
+
+    return this.client.request(settings)
+  }
 }
 
 export default HttpClient

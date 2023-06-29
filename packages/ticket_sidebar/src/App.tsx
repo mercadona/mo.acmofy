f09a93a13476fb9cb30d1e2b1430266b8b04d62a
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-29 13:25:13 +0200

    feat: Replicate previous functionality

--- packages/ticket_sidebar/src/App.tsx
+++ packages/ticket_sidebar/src/App.tsx
@@ -1,20 +1,61 @@
 import * as React from 'react'
+import { Grid, Col, Row } from '@zendeskgarden/react-grid'
+import zafClient from '@app/zendesk/sdk'
+
+import { useConfig } from './context/ConfigProvider'
+import { TicketResponse } from './types'
 
 import './style.css'
 
 const App = () => {
-  const [counter, setCounter] = React.useState(0)
+  const { httpClient, orderIdCustomFieldId, idFieldMinLength } = useConfig()
+  const [orderId, setOrderId] = React.useState<string>()
 
-  const increment = () => setCounter((prev) => prev + 1)
-  const decrement = () => setCounter((prev) => prev - 1)
+  const getTicketInfo = async (orderId: string) => {
+    try {
+      const { ticket }: TicketResponse = await zafClient.get('ticket')
+      await httpClient.complete({
+        orderId,
+        ticketId: ticket.id,
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message)
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    zafClient.on(
+      `ticket.custom_field_${orderIdCustomFieldId}.changed`,
+      (orderId: string) => setOrderId(orderId)
+    )
+  }, [])
+
+  React.useEffect(() => {
+    if (!orderId || orderId.length < idFieldMinLength) return
+    getTicketInfo(orderId)
+  }, [orderId])
 
   return (
-    <main>
-      <h1>Acmofy React test</h1>
-      <p>Counter is: {counter}</p>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </main>
+    <Grid>
+      <Row>
+        <Col size={12}>Pedido {orderId}</Col>
+      </Row>
+      <Row
+        style={{
+          marginTop: 16,
+        }}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Col size={1}></Col>
+        <Col size={4}>Estado</Col>
+        <Col size={7}>
+          <div className="indicator__confirmed"></div>Confirmado
+        </Col>
+      </Row>
+    </Grid>
   )
 }
 

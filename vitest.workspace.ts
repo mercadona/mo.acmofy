ecbec8d41483264789a1581f2c89b099bde5873d
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-29 20:00:45 +0200

    test: Configure vitest workspaces properly

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,10 +1,12 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
-    {
-        test: {
-            include: ['packages/**/tests/**/*.test.ts'],
-            name: 'ACMOFy'
-        }
-    }
+  {
+    test: {
+      globals: true,
+      name: 'ACMOFy',
+      environment: 'jsdom',
+      setupFiles: './packages/ticket_sidebar/setupTests.ts',
+    },
+  },
 ])

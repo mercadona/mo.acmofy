35515f6bacded09b7df5ddcb001d6db6441197ca
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-29 13:24:13 +0200

    feat: Add context to inject settings

--- packages/ticket_sidebar/src/main.tsx
+++ packages/ticket_sidebar/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from '@zendeskgarden/react-theming'
 import App from './App'
+import { ConfigProvider } from './context/ConfigProvider'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ConfigProvider>
   </React.StrictMode>
 )

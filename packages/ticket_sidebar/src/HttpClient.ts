4f34a3694cfe126a754805d0497eba2a8e091096
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-29 13:25:13 +0200

    feat: Replicate previous functionality

--- packages/ticket_sidebar/src/HttpClient.ts
+++ packages/ticket_sidebar/src/HttpClient.ts
@@ -2,6 +2,11 @@ import zafClient from '@app/zendesk/sdk'
 
 type ZendeskClient = typeof zafClient
 
+type TicketInfo = {
+  orderId: string
+  ticketId: number
+}
+
 class HttpClient {
   private client: ZendeskClient
   private zendeskURL: string
@@ -14,7 +19,7 @@ class HttpClient {
   request(data: object): Promise<unknown> {
     const settings = {
       url: this.zendeskURL,
-      type: 'POST',
+      type: 'POST' as const,
       contentType: 'application/json',
       data: JSON.stringify(data),
     }
@@ -25,7 +30,20 @@ class HttpClient {
   get(): Promise<unknown> {
     const settings = {
       url: this.zendeskURL,
-      type: 'GET',
+      type: 'GET' as const,
+    }
+
+    return this.client.request(settings)
+  }
+
+  complete(ticketInfo: TicketInfo): Promise<unknown> {
+    const settings = {
+      url: `${this.zendeskURL}${ticketInfo.ticketId}/complete/`,
+      type: 'POST' as const,
+      contentType: 'application/json',
+      data: JSON.stringify({
+        order_id: ticketInfo.orderId,
+      }),
     }
 
     return this.client.request(settings)

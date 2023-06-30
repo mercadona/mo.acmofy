1610e211dddb487f744fbcbc11cb0aaff9725a62
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-30 09:53:00 +0200

    test: First passing UI test

--- packages/ticket_sidebar/src/tests/App.test.tsx
+++ packages/ticket_sidebar/src/tests/App.test.tsx
@@ -3,32 +3,38 @@ import { render, screen } from '../utils/test-utils'
 
 import App from '../App'
 
-vi.mock('@app/zendesk/sdk', async () => {
-  vi.stubGlobal('ZAFClient', {
-    init: vi.fn(),
-  })
-  return {
-    default: {
-      request: vi.fn(),
-      on: vi.fn(),
-      get: vi.fn().mockResolvedValue({
-        id: '12345',
-      }),
-      metadata: vi.fn().mockResolvedValue({
-        settings: {
-          URL_ZENDESK_HOOK: 'https://example.com/api',
-          MINIMUM_ORDER_ID_LENGTH: 5,
-          ORDER_ID_CUSTOM_FIELD_ID: '1234567',
-        },
-      }),
-    },
-  }
+afterEach(() => {
+  vi.unstubAllGlobals()
 })
 
 describe('Tests for App component', () => {
-  it('should render App', async () => {
-    render(<App />)
+  describe('when app starts', () => {
+    beforeEach(() => {
+      vi.mock('@app/zendesk/sdk', () => {
+        vi.stubGlobal('ZAFClient', {
+          init: vi.fn(),
+        })
+        return {
+          default: {
+            request: vi.fn(),
+            on: vi.fn(),
+            get: vi.fn(),
+            metadata: vi.fn().mockResolvedValue({
+              settings: {
+                URL_ZENDESK_HOOK: 'https://example.com/api',
+                MINIMUM_ORDER_ID_LENGTH: 5,
+                ORDER_ID_CUSTOM_FIELD_ID: '1234567',
+              },
+            }),
+          },
+        }
+      })
+    })
 
-    expect(await screen.findByText(/order/i)).toBeInTheDocument()
+    it('should render App', async () => {
+      render(<App />)
+
+      expect(await screen.findByText(/order/i)).toBeInTheDocument()
+    })
   })
 })

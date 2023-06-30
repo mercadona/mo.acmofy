e2623bd59c6f6635886f69c0fe8ff9210f06272c
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-30 13:06:30 +0200

    feat: Improve HttpClient

--- packages/ticket_sidebar/src/tests/HttpClient.test.ts
+++ packages/ticket_sidebar/src/tests/HttpClient.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi, afterEach } from 'vitest'
-import HttpClient from '../HttpClient'
+import { HttpClient } from '../http'
 import zafClient from '@app/zendesk/sdk'
 
 describe('HttpClient tests', () => {
@@ -14,13 +14,27 @@ describe('HttpClient tests', () => {
     expect(httpClient).toBeInstanceOf(HttpClient)
   })
 
-  it('should call client with correct parameters', async () => {
+  it('should call client with GET and without data', async () => {
+    const zendeskURL = 'https://example.com/api/'
+    const httpClient = new HttpClient(zafClient, zendeskURL)
+
+    await httpClient.request({ path: 'customers/' })
+
+    expect(zafClient.request).toHaveBeenCalledOnce()
+    expect(zafClient.request).toHaveBeenCalledWith({
+      url: 'https://example.com/api/customers/',
+      type: 'GET',
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    })
+  })
+
+  it('should call client with POST method and data', async () => {
     const zendeskURL = 'https://example.com'
     const httpClient = new HttpClient(zafClient, zendeskURL)
 
     const data = { order_id: 1, ticket_id: 2112, acmofy: true }
 
-    await httpClient.request(data)
+    await httpClient.request({ data })
 
     expect(zafClient.request).toHaveBeenCalledOnce()
     expect(zafClient.request).toHaveBeenCalledWith({
@@ -28,19 +42,6 @@ describe('HttpClient tests', () => {
       type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(data),
-    })
-  })
-
-  it('should call client with correct parameters', async () => {
-    const zendeskURL = 'https://example.com'
-    const httpClient = new HttpClient(zafClient, zendeskURL)
-
-    await httpClient.get()
-
-    expect(zafClient.request).toHaveBeenCalledOnce()
-    expect(zafClient.request).toHaveBeenCalledWith({
-      url: 'https://example.com',
-      type: 'GET',
     })
   })
 })

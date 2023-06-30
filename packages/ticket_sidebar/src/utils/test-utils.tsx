c520fda63dff816f9ff6eade799112dca6e39d8c
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-30 09:51:55 +0200

    test: Update util with all providers

--- packages/ticket_sidebar/src/utils/test-utils.tsx
+++ packages/ticket_sidebar/src/utils/test-utils.tsx
@@ -2,13 +2,18 @@ import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import { render } from '@testing-library/react'
 import { ConfigProvider } from '../context/ConfigProvider'
+import { ThemeProvider } from '@zendeskgarden/react-theming'
 
 afterEach(() => {
   cleanup()
 })
 
 const AllProviders = ({ children }: { children: React.ReactElement }) => {
-  return <ConfigProvider>{children}</ConfigProvider>
+  return (
+    <ThemeProvider>
+      <ConfigProvider>{children}</ConfigProvider>
+    </ThemeProvider>
+  )
 }
 
 const customRender = (ui: React.ReactElement) => {

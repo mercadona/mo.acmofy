2e583d0c89358a7a231744ed8a0bd0f209c9c7a6
Ruben S. Garcia <rgarci12@mercadona.es>
2023-07-03 10:00:48 +0200

    chore: Use Indicator component

--- packages/ticket_sidebar/src/components/OrderInfo.tsx
+++ packages/ticket_sidebar/src/components/OrderInfo.tsx
@@ -1,8 +1,34 @@
-// import * as React from 'react'
 import { Grid, Col, Row } from '@zendeskgarden/react-grid'
+import { Indicator, OrderStatus } from './styled'
 
 type OrderInfoProps = {
   orderId: string | undefined
+}
+
+const STATES: Array<{ [key: string]: string }> = [
+  { Checkout: 'checkout' },
+  { Confirmado: 'confirmed' },
+  { Preparando: 'preparing' },
+  { Preparado: 'prepared' },
+  { Entregando: 'delivering' },
+  { Entregado: 'delivered' },
+  { 'Cancelado por el cliente': 'cancelled_by_customer' },
+  { 'Cancelado por el sistema': 'cancelled_by_system' },
+  { 'Usuario no disponible': 'user_unreachable' },
+  { Atrasado: 'delayed' },
+]
+
+const getRandomState = (
+  states: typeof STATES
+): { text: string; value: OrderStatus } => {
+  const idx = Math.floor(Math.random() * Object.keys(states).length)
+
+  const state = Object.keys(STATES[idx])[0]
+
+  return {
+    text: state,
+    value: states[idx][state] as OrderStatus,
+  }
 }
 
 const OrderInfo = ({ orderId }: OrderInfoProps) => {
@@ -19,6 +45,8 @@ const OrderInfo = ({ orderId }: OrderInfoProps) => {
   // }, [orderId])
 
   if (!orderId) return null
+
+  const { text, value } = getRandomState(STATES)
 
   return (
     <Grid>
@@ -45,7 +73,8 @@ const OrderInfo = ({ orderId }: OrderInfoProps) => {
             alignItems: 'center',
           }}
         >
-          <div className="indicator__confirmed"></div>Confirmado
+          <Indicator $type={value} />
+          {text}
         </Col>
       </Row>
     </Grid>

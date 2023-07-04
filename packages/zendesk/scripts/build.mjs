3ccb3f3a5293b588bf2d3f3978f07751d086b88f
Ruben S. Garcia <rgarci12@mercadona.es>
2023-07-04 17:36:50 +0200

    refactor: Update boilerplate scripts

--- packages/zendesk/scripts/build.mjs
+++ packages/zendesk/scripts/build.mjs
@@ -1,87 +1,87 @@
-import fs from "fs-extra";
+import fs from 'fs-extra'
 
-const currentDir = process.cwd();
-const destinationDir = process.env.INIT_CWD + "/dist";
-const env = process.env.ENV;
+const currentDir = process.cwd()
+const destinationDir = process.env.INIT_CWD + '/dist'
+const env = process.env.ENV
 const COPY_LIST = [
-  "assets",
-  "translations",
-  "manifest.json",
-  "zcli.apps.config.json"
-];
+  'assets',
+  'translations',
+  'manifest.json',
+  'zcli.apps.config.json',
+]
 
-const shouldBeCopied = (fileName) => COPY_LIST.includes(fileName);
+const shouldBeCopied = (fileName) => COPY_LIST.includes(fileName)
 
 const copyFile = (fileName) => {
-  const currPath = currentDir + `/${fileName}`;
-  const distPath = destinationDir + `/${fileName}`;
-  fs.copySync(currPath, distPath);
-};
+  const currPath = currentDir + `/${fileName}`
+  const distPath = destinationDir + `/${fileName}`
+  fs.copySync(currPath, distPath)
+}
 
 /**
  * Remove all the properties from manifest that are not needed for production
  */
 const normalizeManifest = (manifest) => {
   if (!manifest) {
-    return;
+    return
   }
-  const manifestCopy = { ...manifest };
+  const manifestCopy = { ...manifest }
   for (const key in manifestCopy.location.support) {
-    const supportApp = manifestCopy.location.support[key];
+    const supportApp = manifestCopy.location.support[key]
 
     // hard coded properties for now
     // if there are more properties in the future - prepare a smarter utility
-    delete supportApp["dev:url"];
-    delete supportApp["dev:port"];
+    delete supportApp['dev:url']
+    delete supportApp['dev:port']
   }
 
-  return manifestCopy;
-};
+  return manifestCopy
+}
 
 /**
  * Prepare manifest file - if this is a local environment, serve UI files from localhost
  * Otherwise, serve files from build folders for each respective app
  */
 const handleManifest = (fileName) => {
-  const manifest = fs.readJsonSync(fileName);
+  const manifest = fs.readJsonSync(fileName)
   for (const appKey in manifest.location.support) {
-    const app = manifest.location.support[appKey];
-    const devUrl = app["dev:url"] ?? "localhost";
-    const devPort = app["dev:port"] ?? "3000";
+    const app = manifest.location.support[appKey]
+    const devUrl = app['dev:url'] ?? 'localhost'
+    const devPort = app['dev:port'] ?? '3000'
 
     manifest.location.support[appKey] = {
       ...manifest.location.support[appKey],
       url:
-        env === "local"
+        env === 'local'
           ? `http://${devUrl}:${devPort}`
           : manifest.location.support[appKey].url,
-    };
+    }
   }
 
-  const cleanManifest = normalizeManifest(manifest);
+  const cleanManifest = normalizeManifest(manifest)
   fs.writeJsonSync(`${destinationDir}/${fileName}`, cleanManifest, {
-    spaces: "\t",
-  });
-};
+    spaces: '\t',
+  })
+}
 
 const build = () => {
-  fs.ensureDirSync(destinationDir);
-  fs.emptyDirSync(destinationDir);
+  fs.ensureDirSync(destinationDir)
+  fs.emptyDirSync(destinationDir)
 
   fs.readdir(currentDir, (err, files) => {
     if (err) {
-      throw Error(err);
+      throw Error(err)
     }
     for (const file of files) {
       if (shouldBeCopied(file)) {
-        copyFile(file);
+        copyFile(file)
       }
 
-      if (file === "manifest.json") {
-        handleManifest(file);
+      if (file === 'manifest.json') {
+        handleManifest(file)
       }
     }
-  });
-};
+  })
+}
 
-build();
+build()

25cc6bc6d609a1c43b35daed17a9303f232e1db5
Ruben S. Garcia <rgarci12@mercadona.es>
2023-07-10 15:40:59 +0200

    test: Unit tests for OrderStatus

---
+++ packages/ticket_sidebar/src/tests/OrderStatus.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '../utils/test-utils'
+
+import OrderStatus from '../components/OrderStatus'
+
+describe('Tests for OrderStatus', () => {
+  it.each`
+    status                     | expected
+    ${'checkout'}              | ${'Checkout in progress'}
+    ${'confirmed'}             | ${'Confirmed'}
+    ${'preparing'}             | ${'Preparing'}
+    ${'prepared'}              | ${'Prepared'}
+    ${'delivering'}            | ${'Delivering'}
+    ${'delivered'}             | ${'Delivered'}
+    ${'cancelled-by-customer'} | ${'Cancelled by customer'}
+    ${'cancelled-by-system'}   | ${'Cancelled by system'}
+    ${'user-unreachable'}      | ${'User unreachable'}
+    ${'delayed'}               | ${'Delayed'}
+  `('should render OrderStatus properly', ({ status, expected }) => {
+    const order = {
+      status,
+      phone_country_code: 34,
+      phone_number: 666543442,
+    }
+    render(<OrderStatus order={order} />, { wrapper: undefined })
+
+    expect(screen.getByText(expected)).toBeInTheDocument()
+  })
+})

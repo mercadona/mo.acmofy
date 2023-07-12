93b58cc1c0adcad1789c85918ab1d7f076d9deb6
Ruben S. Garcia <rgarci12@mercadona.es>
2023-07-12 12:24:58 +0200

    test: Simplify tests

--- packages/ticket_sidebar/src/tests/OrderInfo.test.tsx
+++ packages/ticket_sidebar/src/tests/OrderInfo.test.tsx
@@ -5,29 +5,7 @@ import { Mocked, expect } from 'vitest'
 
 import OrderInfo from '../components/OrderInfo'
 
-vi.mock('@app/zendesk/sdk', () => {
-  return {
-    default: {
-      metadata: vi.fn().mockResolvedValue({
-        settings: {
-          URL_ZENDESK_HOOK: 'https://example.com/api',
-          MINIMUM_ORDER_ID_LENGTH: 5,
-          ORDER_ID_CUSTOM_FIELD_ID: '1234567',
-        },
-      }),
-      get: vi.fn().mockImplementation((what: string) => {
-        if (what === 'currentUser.email') {
-          return Promise.resolve({
-            errors: [],
-            'currentUser.email': 'sgarcal123456@mercadona.es',
-          })
-        }
-      }),
-      on: vi.fn().mockReturnValue(true),
-      request: vi.fn().mockResolvedValue(true),
-    },
-  }
-})
+vi.mock('@app/zendesk/sdk')
 
 vi.stubGlobal('console', {
   log: vi.fn(),
